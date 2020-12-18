62831cae3315f72872f05ce4a12716b4c0a06672
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2020-12-18 15:50:33 +0200

    Add logging

--- numba_mcerd/main.py
+++ numba_mcerd/main.py
@@ -1,8 +1,32 @@
+import logging
+
 from numba_mcerd.mcerd import random
 
 
+def setup_logging():
+    """Setup logging for all modules"""
+    level = logging.DEBUG
+    filename = "numba_mcerd.log"
+    # Format example: "2020-12-18 15:34:26 DEBUG    [main.py:12] Initializing variables"
+    logging_format = "{asctime} {levelname:8} [{filename}:{lineno}] {message}"
+    style = "{"
+    date_format = "%Y-%m-%d %H:%M:%S"
+
+    # PyCharm doesn't recognize the 'style' keyword
+    # noinspection PyArgumentList
+    logging.basicConfig(filename=filename, level=level, style=style,
+                        format=logging_format, datefmt=date_format)
+
+
 def main():
+    # Misc setup
+
     random.seed_rnd(1)
+    setup_logging()
+
+    # Variables
+
+    logging.debug("Initializing variables")
 
 
 if __name__ == '__main__':
