928e6a18075f5e59cf24037d808c2d36f611f291
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2021-01-17 20:43:43 +0200

    Implement output.output_data

--- numba_mcerd/mcerd/output.py
+++ numba_mcerd/mcerd/output.py
@@ -1,3 +1,5 @@
+import logging
+
 import numba_mcerd.mcerd.constants as c
 import numba_mcerd.mcerd.objects as o
 import numba_mcerd.mcerd.symbols as s
@@ -8,7 +10,16 @@ def output_erd(g: o.Global, cur_ion: o.Ion, target: o.Target, detector: o.Detect
 
 
 def output_data(g: o.Global) -> None:
-    raise NotImplementedError
+    """Output how many ions have been calculated"""
+    if g.simstage == c.SimStage.PRESIMULATION:
+        nion = g.cion
+        nmaxion = g.npresimu
+    else:
+        nion = g.cion - g.npresimu
+        nmaxion = g.nsimu - g.npresimu
+
+    if nmaxion > 100 and nion % (nmaxion // 100) == 0:
+        logging.info(f"Calculated {nion} of {nmaxion} ions {100.0 * nion / nmaxion:.0}%")
 
 
 def output_trackpoint(
