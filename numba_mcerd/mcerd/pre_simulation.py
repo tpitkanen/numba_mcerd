86c95c22c183451edbda5ebeecdbf7791fbcb9bd
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2021-01-21 11:47:43 +0200

    Implement finish_presimulation

--- numba_mcerd/mcerd/pre_simulation.py
+++ numba_mcerd/mcerd/pre_simulation.py
@@ -2,13 +2,26 @@ from typing import Tuple
 
 import numba_mcerd.mcerd.constants as c
 import numba_mcerd.mcerd.objects as o
-
+from numba_mcerd.mcerd import rotate
 
 NPRESIMU = 500
 
 
 def finish_presimulation(g: o.Global, detector: o.Detector, recoil: o.Ion) -> None:
-    raise NotImplementedError
+    """In the presimulation stage we save the recoil angle relative to the
+    detector when the recoil comes out of the target. Also the recoil
+    depth and layer are saved for later use.
+    """
+    # Recoil direction in the laboratory coordinate system
+    theta_lab, fii_lab = rotate.rotate(recoil.lab.theta, recoil.lab.fii, recoil.theta, recoil.fii)
+
+    # Recoil direction in the detector coordinate system
+    theta, fii = rotate.rotate(detector.angle, c.C_PI, theta_lab, fii_lab)
+
+    g.presimu[g.cpresimu].depth = recoil.hist.tar_recoil.p.z
+    g.presimu[g.cpresimu].angle = theta
+    g.presimu[g.cpresimu].layer = recoil.hist.layer
+    g.cpresimu += 1
 
 
 def analyze_presimulation(g: o.Global, target: o.Target, detector: o.Detector) -> None:
