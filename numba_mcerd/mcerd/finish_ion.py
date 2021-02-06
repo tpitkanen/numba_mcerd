15689fa39b738d78b90945483ad638229166dd63
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2021-02-06 19:43:33 +0200

    Implement finish_ion.py

--- numba_mcerd/mcerd/finish_ion.py
+++ numba_mcerd/mcerd/finish_ion.py
@@ -1,7 +1,11 @@
 import numba_mcerd.mcerd.constants as c
 import numba_mcerd.mcerd.objects as o
-import numba_mcerd.mcerd.symbols as s
 
 
 def finish_ion(g: o.Global, ion: o.Ion) -> None:
-    raise NotImplementedError
+    """Output information about stopped or transmitted ions to g.master.fprange"""
+    # TODO: Problematic for multi-threading, and possibly slow
+    if ion.status == c.IonStatus.FIN_STOP:
+        g.master.fprange.write_text(f"R {ion.p.z / c.C_NM:10.3f}\n")
+    if ion.status == c.IonStatus.FIN_TRANS:
+        g.master.fprange.write_text(f"T {ion.E / c.C_MEV:12.6f}\n")
