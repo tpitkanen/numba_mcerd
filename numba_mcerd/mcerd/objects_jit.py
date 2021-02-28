5e51960bbb064e5fe022116e8258cff73a646f28
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2021-03-01 00:55:39 +0200

    Add unused jitclass Target, misc updates

--- numba_mcerd/mcerd/objects_jit.py
+++ numba_mcerd/mcerd/objects_jit.py
@@ -1,14 +1,24 @@
 """Objects from general.h adapted to jitclass"""
 
 import numpy as np
+import numba as nb
 from numba import int64, float64
 from numba.experimental import jitclass
+
+from numba_mcerd.mcerd import constants
 
 
 # TODO: Jitclasses can be nested:
 #       https://stackoverflow.com/questions/57640039/numba-jit-and-deferred-types
 #       https://stackoverflow.com/questions/38682260/how-to-nest-numba-jitclass
 #       https://stackoverflow.com/questions/59215076/how-make-a-python-class-jitclass-compatible-when-it-contains-itself-jitclass-cla
+
+# TODO: How to use Enums?
+#       https://stackoverflow.com/questions/61155860/can-i-compile-a-python-enum-into-a-numba-jitclass
+
+# TODO: How to create empty lists? Creating with one element and then
+#       clearing feels like a workaround.
+#       https://stackoverflow.com/questions/58412982/using-lists-in-numba-jitclass
 
 # Tuple-style definition
 # @jitclass([
@@ -25,10 +35,10 @@ from numba.experimental import jitclass
     "z": float64,
 })
 class Point:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
 
 
 @jitclass({
@@ -36,9 +46,9 @@ class Point:
     "y": float64
 })
 class Point2:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
 
 
 # TODO: float64?
@@ -108,26 +118,119 @@ class Potential:
 #     pass
 #
 #
-# class Target_ele:
-#     pass
-#
-#
-# class Target_sto:
-#     pass
-#
-#
-# class Target_layer:
-#     pass
-#
-#
-# class Plane:
-#     pass
-#
-#
-# class Target:
-#     pass
-#
-#
+
+@jitclass({
+    "Z": float64,
+    "A": float64
+})
+class Target_ele:
+    """Target element"""
+    def __init__(self):
+        self.Z = 0.0
+        self.A = 0.0
+
+
+@jitclass({
+    "vel": float64[:],
+    "sto": float64[:],
+    "stragg": float64[:],
+    "stodiv": float64,
+    "n_sto": int64
+})
+class Target_sto:
+    """Target stopping values"""
+    def __init__(self):
+        self.vel = np.zeros(constants.MAXSTO, dtype=np.float64)
+        self.sto = np.zeros(constants.MAXSTO, dtype=np.float64)
+        self.stragg = np.zeros(constants.MAXSTO, dtype=np.float64)
+        self.stodiv = 0.0
+        self.n_sto = 0
+
+
+@jitclass({
+    "natoms": int64,
+    "dlow": float64,
+    "dhigh": float64,
+    "atom": int64[:],
+    "N": float64[:],
+    "Ntot": float64,
+    # "sto": nb.types.List(Target_sto.class_type.instance_type),
+    "type": int64,  # constants.TargetType
+    "gas": nb.boolean,
+    "stofile_prefix": nb.types.string
+})
+class Target_layer:
+    """Target layer"""
+    def __init__(self):
+        self.natoms = 0  # Number of different elements in this layer
+        self.dlow = 0.0  # lower depth of the target layer
+        self.dhigh = 0.0  # higher depth of the target layer
+        self.atom = np.zeros(constants.MAXATOMS, dtype=np.int64)  # Array of the indices of the target elements
+        self.N = np.zeros(constants.MAXATOMS, dtype=np.float64)  # Array of the atomic densities
+        self.Ntot = 0.0  # Total atomic density in the layer
+        # self.sto = [Target_sto()]  # Electronic stopping for different ions  # len g.nions
+        self.type = -1  # Type of the target layer
+        self.gas = False  # Whether the target layer is gas or not
+        self.stofile_prefix = ""  # len MAXSTOFILEPREFIXLEN
+
+        # self.sto.clear()  # Needed to clear
+
+
+@jitclass({
+    "a": float64,
+    "b": float64,
+    "c": float64,
+    "type": int64  # constants.PlaneType
+})
+class Plane:
+    def __init__(self):
+        self.a = 0.0
+        self.b = 0.0
+        self.c = 0.0
+        self.type = -1
+
+
+@jitclass({
+    "minN": float64,
+    "ele": nb.types.List(Target_ele.class_type.instance_type),
+    "layer": nb.types.List(Target_layer.class_type.instance_type),
+    "nlayers": int64,
+    "ntarget": int64,
+    "natoms": int64,
+    "recdist": nb.types.List(Point2.class_type.instance_type),
+    "nrecdist": int64,
+    "effrecd": float64,
+    "recmaxd": float64,
+    "plane": Plane.class_type.instance_type,
+    "efin": float64[:],
+    "recpar": nb.types.List(Point2.class_type.instance_type),
+    "angave": float64,
+    # "surface": Surface.class_type.instance_type,
+    "cross": float64[:, :],
+    "table": nb.boolean
+})
+class Target:
+    def __init__(self):
+        self.minN = 0.0  # Minimum atomic density in the target
+        self.ele = [Target_ele() for _ in range(constants.MAXELEMENTS)]  # Array of different elements in the target  # len MAXELEMENTS
+        self.layer = [Target_layer() for _ in range(constants.MAXLAYERS)]  # Array of the target layers  # len MAXLAYERS
+        self.nlayers = 0  # Total number of target layers
+        self.ntarget = 0  # Number of the layers in the target itself
+        self.natoms = 0  # Total number of atoms in different target layers
+        self.recdist = [Point2() for _ in range(constants.NRECDIST)]  # Recoil material distribution in the target  # len NRECDIST
+        self.nrecdist = 0  # Number of points in recoil material distribution
+        self.effrecd = 0.0  # Effective thickness of recoil material (nonzero values)
+        self.recmaxd = 0.0  # Maximum depth of the recoiling material
+        self.plane = Plane()  # Target surface plane
+        self.efin = np.zeros(constants.NRECDIST, dtype=np.float64)  # Energies below which the recoil can't reach the surface  # len NRECDIST
+        self.recpar = [Point2() for _ in range(constants.MAXLAYERS)]  # Recoiling solid angle fitted parameters  # len MAXLAYERS
+        self.angave = 0.0  # Average recoiling half-angle
+        # self.surface = Surface()  # Data structure for the rough surface
+        # len [NSENE][NSANGLE]:
+        self.cross = np.zeros((constants.NSENE, constants.NSANGLE), dtype=np.float64)  # Cross section table for scattering relative to the Rutherford cross sections as a function of ion lab. energy and lab scattering angle
+        self.table = False  # True if cross section table is available
+
+
 # class Line:
 #     pass
 #
@@ -146,4 +249,14 @@ class Potential:
 #
 # class Detector:
 #     pass
+
+
+
+def main():
+    target = Target()
+    print(target.layer[0].atom)
+
+
+if __name__ == '__main__':
+    main()
 
