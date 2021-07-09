a0b6502db8d8791c86f4797043a9ebaf74f6d310
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2021-07-09 13:23:29 +0300

    Add finalize.py

--- numba_mcerd/mcerd/finalize.py
+++ numba_mcerd/mcerd/finalize.py
@@ -1,7 +1,16 @@
-import numba_mcerd.mcerd.constants as c
 import numba_mcerd.mcerd.objects as o
-import numba_mcerd.mcerd.symbols as s
+from numba_mcerd.mcerd import enums
 
 
 def finalize(g: o.Global) -> None:
-    raise NotImplementedError
+    """Output statistics of ion finishes to g.master.fpout"""
+    dat_lines = ["Statistics of ion finishes: "]
+
+    for i in range(enums.IonType.SECONDARY.value + 1):
+        for j in range(len(enums.IonStatus)):
+            dat_lines.append(f"{i:2d} {j:2d}: {g.finstat[i][j]:5d}")
+
+    if g.simtype == enums.SimType.SIM_RBS:
+        raise NotImplementedError
+
+    g.master.fpdat.write_text("\n".join(dat_lines))
