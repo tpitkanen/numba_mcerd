8a15e5007684ef7893aaa6e18468be7cdd471a44
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2021-09-05 21:06:59 +0300

    Add _o to original object names, remove _d from dtype objects

--- numba_mcerd/main_jit.py
+++ numba_mcerd/main_jit.py
@@ -49,15 +49,15 @@ def main(args):
 
     initialization_timer = timer.SplitTimer.init_and_start()
 
-    g = o.Global()
-    primary_ion = o.Ion()  # Not really used in the simulation loop
-    secondary_ion = o.Ion()  # Not really used in the simulation loop
-    previous_trackpoint_ion = o.Ion()  # Not used unless simulation is RBS
-    ions_moving = []  # TODO: Initialize a list of ions?
-    target = o.Target()
-    scat = None
-    snext = o.SNext()
-    detector = o.Detector()
+    g_o = o.Global()
+    primary_ion_o = o.Ion()  # Not really used in the simulation loop
+    secondary_ion_o = o.Ion()  # Not really used in the simulation loop
+    previous_trackpoint_ion_o = o.Ion()  # Not used unless simulation is RBS
+    ions_moving_o = []
+    target_o = o.Target()
+    scat_o = None
+    snext_o = o.SNext()
+    detector_o = o.Detector()
 
     i = None
     j = None
@@ -75,29 +75,29 @@ def main(args):
 
     logging.debug("Starting preprocessing")
     logging.info("Initializing Jibal")
-    g.jibal.initialize()
+    g_o.jibal.initialize()
     logging.info("Initializing parameters")
-    init_params.init_params(g, target, args)
+    init_params.init_params(g_o, target_o, args)
 
     logging.info("Initializing input files")
-    read_input.read_input(g, primary_ion, secondary_ion, previous_trackpoint_ion, target, detector)
+    read_input.read_input(g_o, primary_ion_o, secondary_ion_o, previous_trackpoint_ion_o, target_o, detector_o)
 
     # TODO: read_input needlessly seeds built-in random
     # TODO: Non-JIT modules may use random_vanilla, how to fix this without
     #       excessively copy-pasting JIT-versions of each module?
-    random_jit.seed_rnd(g.seed)
+    random_jit.seed_rnd(g_o.seed)
 
     # Package ions into an array. Ions are (sometimes) accessed in the
     # original code like this
-    if g.nions == 2:
-        ions = [primary_ion, secondary_ion]
-    elif g.nions == 3:
-        ions = [primary_ion, secondary_ion, previous_trackpoint_ion]
+    if g_o.nions == 2:
+        ions = [primary_ion_o, secondary_ion_o]
+    elif g_o.nions == 3:
+        ions = [primary_ion_o, secondary_ion_o, previous_trackpoint_ion_o]
     else:
         raise NotImplementedError
 
     logging.info("Initializing output files")
-    init_params.init_io(g, primary_ion, target)
+    init_params.init_io(g_o, primary_ion_o, target_o)
 
     # TODO: Rename to pot(ential)
     # Vanilla Python:
@@ -108,65 +108,65 @@ def main(args):
     #       easier when using .py_func()?
     pot = potential_jit.make_screening_table_dtype()
 
-    ion_stack.cascades_create_additional_ions(g, detector, target, [])
+    ion_stack.cascades_create_additional_ions(g_o, detector_o, target_o, [])
 
-    logging.info(f"{g.nions} ions, {target.natoms} target atoms")
+    logging.info(f"{g_o.nions} ions, {target_o.natoms} target atoms")
 
     # (g.jibal.gsto.extrapolate = True)
 
     table_timer = timer.SplitTimer.init_and_start()
-    scat = []
-    for i in range(g.nions):
-        if g.simtype == enums.SimType.RBS and i == enums.IonType.TARGET_ATOM.value:
+    scat_o = []
+    for i in range(g_o.nions):
+        if g_o.simtype == enums.SimType.RBS and i == enums.IonType.TARGET_ATOM.value:
             continue
         ions[i].scatindex = i
-        scat.append([o.Scattering() for _ in range(c.MAXELEMENTS)])
-        for j in range(target.natoms):
-            init_simu_jit.scattering_table(g, ions[i], target, scat[i][j], pot, j)
-            cross_section_jit.calc_cross_sections(g, scat[i][j], pot)
+        scat_o.append([o.Scattering() for _ in range(c.MAXELEMENTS)])
+        for j in range(target_o.natoms):
+            init_simu_jit.scattering_table(g_o, ions[i], target_o, scat_o[i][j], pot, j)
+            cross_section_jit.calc_cross_sections(g_o, scat_o[i][j], pot)
 
     table_timer.stop()
     print(f"table_timer: {table_timer}")
 
     gsto_index = -1
-    for j in range(target.nlayers):
-        target.layer[j].sto = [o.Target_sto() for _ in range(g.nions)]
-        for i in range(g.nions):
+    for j in range(target_o.nlayers):
+        target_o.layer[j].sto = [o.Target_sto() for _ in range(g_o.nions)]
+        for i in range(g_o.nions):
             gsto_index += 1
-            if g.simtype == enums.SimType.RBS and i == enums.IonType.TARGET_ATOM.value:
+            if g_o.simtype == enums.SimType.RBS and i == enums.IonType.TARGET_ATOM.value:
                 continue
-            elsto.calc_stopping_and_straggling_const(g, ions[i], target, j, gsto_index)
+            elsto.calc_stopping_and_straggling_const(g_o, ions[i], target_o, j, gsto_index)
             # TODO: Real gsto instead
 
-    init_detector.init_detector(g, detector)
+    init_detector.init_detector(g_o, detector_o)
 
-    if g.predata:
-        init_params.init_recoiling_angle(target)
+    if g_o.predata:
+        init_params.init_recoiling_angle(target_o)
 
-    trackid = int(ions[SECONDARY].Z) * 1_000 + g.seed % 1_000
+    trackid = int(ions[SECONDARY].Z) * 1_000 + g_o.seed % 1_000
     trackid *= 1_000_000
-    ions_moving.append(copy.deepcopy(ions[PRIMARY]))
-    ions_moving.append(copy.deepcopy(ions[SECONDARY]))
-    if g.simtype == enums.SimType.RBS:
-        ions_moving.append(copy.deepcopy(ions[TARGET_ATOM]))
+    ions_moving_o.append(copy.deepcopy(ions[PRIMARY]))
+    ions_moving_o.append(copy.deepcopy(ions[SECONDARY]))
+    if g_o.simtype == enums.SimType.RBS:
+        ions_moving_o.append(copy.deepcopy(ions[TARGET_ATOM]))
 
     logging.info("Converting objects to JIT")
 
     initialization_timer.stop()
     print(f"initialization_timer: {initialization_timer}")
 
-    for ion in ions_moving:
+    for ion in ions_moving_o:
         ion.status = enums.IonStatus.NOT_FINISHED
 
     # dtype conversions
     dtype_conversion_timer = timer.SplitTimer.init_and_start()
 
-    g_d = ocd.convert_global(copy.deepcopy(g))
-    ions_moving_d = np.array([ocd.convert_ion(copy.deepcopy(ion)) for ion in ions_moving])
-    target_d = ocd.convert_target(copy.deepcopy(target))
-    scat_d = ocd.convert_scattering_nested(copy.deepcopy(scat))
-    snext_d = ocd.convert_snext(copy.deepcopy(snext))
-    detector_d = ocd.convert_detector(copy.deepcopy(detector))
+    g = ocd.convert_global(copy.deepcopy(g_o))
+    ions_moving = np.array([ocd.convert_ion(copy.deepcopy(ion)) for ion in ions_moving_o])
+    target = ocd.convert_target(copy.deepcopy(target_o))
+    scat = ocd.convert_scattering_nested(copy.deepcopy(scat_o))
+    snext = ocd.convert_snext(copy.deepcopy(snext_o))
+    detector = ocd.convert_detector(copy.deepcopy(detector_o))
 
     dtype_conversion_timer.stop()
     print(f"dtype_conversion_timer: {dtype_conversion_timer}")
@@ -174,12 +174,12 @@ def main(args):
     # Jitclass conversions
     # jitclass_conversion_timer = timer.SplitTimer.init_and_start()
     #
-    # g = ocj.convert_global(g)
-    # ions_moving = [ocj.convert_ion(ion) for ion in ions_moving]
-    # target = ocj.convert_target(target)
-    # scat = ocj.convert_scattering_nested(scat)
-    # snext = ocj.convert_snext(snext)
-    # detector = ocj.convert_detector(detector)
+    # g = ocj.convert_global(g_o)
+    # ions_moving = [ocj.convert_ion(ion_o) for ion in ions_moving]
+    # target = ocj.convert_target(target_o)
+    # scat = ocj.convert_scattering_nested(scat_o)
+    # snext = ocj.convert_snext(snext_o)
+    # detector = ocj.convert_detector(detector_o)
     #
     # jitclass_conversion_timer.stop()
     # print(f"jitclass_conversion_timer: {jitclass_conversion_timer}")
@@ -188,28 +188,28 @@ def main(args):
 
     # TODO: Move this to a separate jit-compiled function to eliminate
     #       context-switching overhead
-    for i in range(g_d.nsimu):
+    for i in range(g.nsimu):
         print(i)
 
-        g_d.cion = i  # TODO: Replace/remove for MT
+        g.cion = i  # TODO: Replace/remove for MT
 
         outer_loop_count = 0
         inner_loop_count = 0
 
         # output.output_data(g)  # Only prints status info
 
-        cur_ion = ions_moving_d[PRIMARY]
+        cur_ion = ions_moving[PRIMARY]
 
-        ion_simu_jit.create_ion(g_d, cur_ion, target_d)
-        if g_d.rough:
-            ion_simu_jit.move_target(target_d)
+        ion_simu_jit.create_ion(g, cur_ion, target)
+        if g.rough:
+            ion_simu_jit.move_target(target)
 
         primary_finished = False
         while not primary_finished:
             outer_loop_count += 1
 
-            ion_simu_jit.next_scattering(g_d, cur_ion, target_d, scat_d, snext_d)
-            nscat = ion_simu_jit.move_ion(g_d, cur_ion, target_d, snext_d)
+            ion_simu_jit.next_scattering(g, cur_ion, target, scat, snext)
+            nscat = ion_simu_jit.move_ion(g, cur_ion, target, snext)
 
     # TODO
     pass
