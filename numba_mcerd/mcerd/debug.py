7acce8c6b26a9b4b60fbede2114f9ca68d76bc7b
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2021-11-14 14:57:11 +0200

    Update debug.py
    - add out_convert and numpy_to_dict
    - improve example call
    - shorten names

--- numba_mcerd/mcerd/debug.py
+++ numba_mcerd/mcerd/debug.py
@@ -3,15 +3,33 @@ def print_ion_position(g, ion, label, flag):
     raise NotImplementedError
 
 
-def objmode_out(*args, **kwargs) -> None:
+def out(*a, **k) -> None:
     """Helper function for debugging with numba.objmode context manager"""
     # simulation_loop parameters:
-    # debug.objmode_out(g=g, master=master, ions=ions, target=target, scat=scat, snext=snext, detector=detector, trackid=trackid, ion_i=ion_i, new_track=new_track)
+    # debug.objmode_out("simulation_loop", g=g, master=master, ions=ions, target=target, scat=scat, snext=snext,
+    #                   detector=detector, trackid=trackid, ion_i=ion_i, new_track=new_track)
     pass
 
 
-def objmode_print(*args, **kwargs) -> None:
+def out_print(*a, **k) -> None:
     """Helper function for printing debug info with numba.objmode context manager"""
-    print(f"args: {args}")
-    print(f"kwargs: {kwargs}")
+    print(f"args: {a}")
+    print(f"kwargs: {k}")
 
+
+def out_convert(*a, **k) -> None:
+    """Helper function for debugging with numba.objmode context manager that
+    converts objects to dict
+    """
+    a_dicts = [numpy_to_dict(obj) for obj in a]
+    k_dicts = {key: numpy_to_dict(k[key]) for key in k}
+    pass
+
+
+def numpy_to_dict(obj):
+    """Convert numpy object to dict. Doesn't affect inner objects."""
+    if not hasattr(obj, "dtype"):
+        return obj
+    d = {name: obj[name] for name in obj.dtype.names}
+    # Sort keys for debugger
+    return {key: d[key] for key in sorted(d)}
